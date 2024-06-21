73e33961826af2091e79aabeec8d1b84a83303cf
Shanur Anasari <32862274+shanurAnsari@users.noreply.github.com>
2024-06-21 17:51:15 +0530

    Delete Customer-rewards-program directory

--- Customer-rewards-program/src/main/java/com/customer/rewards/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.customer.rewards.repository;
-
-import com.customer.rewards.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-     Customer findByCustomerId(Long customerId);
-}
