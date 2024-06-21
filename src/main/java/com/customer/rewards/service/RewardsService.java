035a521e8af1138f37e6c0a1755588260fa196dd
Shanur Anasari <32862274+shanurAnsari@users.noreply.github.com>
2024-06-21 17:33:46 +0530

    Delete src directory

--- src/main/java/com/customer/rewards/service/RewardsService.java
+++
@@ -1,7 +0,0 @@
-package com.customer.rewards.service;
-
-import com.customer.rewards.model.RewardDetails;
-
-public interface RewardsService {
-    public RewardDetails getRewardsOfCustomer(Long customerId);
-}
